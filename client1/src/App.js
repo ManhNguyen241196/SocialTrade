46da9e5ab374585929ffb5696b36138254acfe3e
ManhNguyen241196 <113527026+ManhNguyen241196@users.noreply.github.com>
2023-07-03 21:23:13 +0700

    Add files via upload

---
+++ client1/src/App.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
+import "./App.css";
+
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/register",
+      element: <Register />,
+    },
+  ]);
+
+  return (
+    <div className="App">
+      <React.StrictMode>
+        <RouterProvider router={router} />
+      </React.StrictMode>
+    </div>
+  );
+}
+
+export default App;
---
+++ client1/src/pages/Home/Home.js
@@ -0,0 +1,6 @@
+import "./Home.css";
+const Home = () => {
+  return <div className="home">this is home</div>;
+};
+
+export default Home;
