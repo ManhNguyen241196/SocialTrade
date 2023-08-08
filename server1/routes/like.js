410390a9cdfe734e70c0a3e41575c6de1a419ded
ManhNguyen241196 <113527026+ManhNguyen241196@users.noreply.github.com>
2023-08-08 21:32:32 +0700

    Delete server1 directory

--- server1/routes/like.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-const router = express.Router();
-
-router.post("/", async (req, res, next) => {
-  console.log(`${req.body.userLike} click like`);
-});
-
-export default router;
